54b226e1997c9ea602fc088dae74b8a5ae578be2
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2024-11-19 02:34:47 +0700

    Fix bugg

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ChallengeTopics from './components/challenges/ChallengeTopics';
 import Template from './components/Template';
 import TicketList from './components/ticket/TicketListPage';
 import TicketDetailPage from './components/ticket/TicketDetailPage';
+import Scoreboard from './components/scoreboard/Scoreboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,8 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Template />} />
         <Route path="/login" element={<LoginComponent />} />
-        <Route path="/rankings" element={<Template />} />
-        
+        <Route path="/rankings" element={
+          <Template>
+            <Scoreboard />
+          </Template>} />
         <Route path="/topics" element={
           <Template>
             <ChallengeTopics />
--- src/constants/ApiConstant.jsx
+++ src/constants/ApiConstant.jsx
@@ -22,6 +22,9 @@ export const APi_GET_CHALLENGES_HINTS= '/api/v1/hints'   //+id
 export const API_UNLOCK_HINTS= '/api/v1/unlocks'  //unlock hint
 export const API_GET_UNLOCKED_HINT= '/api/v1/unlocks'  //get unlock
 
+//Scoreboard
+export const API_SCOREBOARD_TOP_STANDINGS= '/api/v1/scoreboard'
+
 //Ticket
 export const API_LIST_TICKET= '/api/tickets-user'
 export const API_DETAIL_TICKET= '/api/tickets'  //details
